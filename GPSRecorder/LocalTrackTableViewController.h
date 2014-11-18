f0418a7e19cc5a11864aa6323d259a8169452beb
givemeakiss518 <jerryzhang518@gmail.com>
2014-11-19 01:02:46 +0800

    显示Document文件夹下的gpx文件列表

---
+++ GPSRecorder/LocalTrackTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocalTrackTableViewController.h
+//  GPSRecorder
+//
+//  Created by zhangchao on 14/11/19.
+//  Copyright (c) 2014年 zhangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalTrackTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *mLocalTrackTableView;
+@property (strong, nonatomic) NSMutableArray *gpxs;
+
+@end
---
+++ GPSRecorder/LocalTrackTableViewController.m
@@ -0,0 +1,131 @@
+//
+//  LocalTrackTableViewController.m
+//  GPSRecorder
+//
+//  Created by zhangchao on 14/11/19.
+//  Copyright (c) 2014年 zhangchao. All rights reserved.
+//
+
+#import "LocalTrackTableViewController.h"
+
+@interface LocalTrackTableViewController ()
+
+@end
+
+@implementation LocalTrackTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    _mLocalTrackTableView.delegate = self;
+    _mLocalTrackTableView.dataSource = self;
+    self.gpxs = [@[] mutableCopy];
+    
+    dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // enumerate all GPX files in Documents directory of application
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *dirPath = paths[0];
+        NSArray *filepaths = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:dirPath error:nil];
+        NSLog(@"zczc filepaths : %@", filepaths);
+        for (NSString *path in filepaths) {
+            if ([path hasSuffix:@".gpx"]) {
+                NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/%@", dirPath, path]];
+                [self.gpxs addObject:url];
+            }
+        }
+        
+        // sort our paths
+        [self.gpxs sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            NSURL *url1 = (NSURL*)obj1;
+            NSURL *url2 = (NSURL*)obj2;
+            return [[url1 absoluteString] compare:[url2 absoluteString]] == NSOrderedAscending;
+        }];
+        
+        // call back on main thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.mLocalTrackTableView reloadData];
+        });
+    });
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return self.gpxs.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellID = @"ID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    NSURL *gpxURL = self.gpxs[indexPath.row];
+    cell.textLabel.text = [gpxURL lastPathComponent];
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
