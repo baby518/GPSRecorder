ab883970da04f28a58f20e1a6606f4f7642229df
ZhangChao <zhangchao@mac_mini.com>
2015-01-03 22:43:35 +0800

    fix bug： 时间格式不对导致GPX文件无法正确导入； 解决： 时区设置为UTC；

--- GPSRecorder/gpx-parser-function/GPXSchema.m
+++ GPSRecorder/gpx-parser-function/GPXSchema.m
@@ -56,14 +56,12 @@ int const MAX_ELEMENT_COUNTS_OF_TRACK               = 100;
 
 + (NSString *) convertTime2String:(NSDate *)time {
     NSDateFormatter *timeFormatter = [[NSDateFormatter alloc] init];
-    //2013-11-02T15:34:49Z
-    //@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'"
-    [timeFormatter setDateFormat:@"yyyy'-'MM'-'dd'-'HH':'mm':'ss'.'SSS"];
+
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"UTC"];
+    [timeFormatter setTimeZone:timeZone];
+
+    [timeFormatter setDateFormat:@"yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'SSS'Z'"];
     NSString *dateString = [timeFormatter stringFromDate:time];
-    if ([dateString length] == 0) {
-        [timeFormatter setDateFormat:@"yyyy'-'MM'-'dd'-'HH':'mm':'ss'"];
-        dateString = [timeFormatter stringFromDate:time];
-    }
     return dateString;
 }
 @end
