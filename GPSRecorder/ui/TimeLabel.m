e786002930dd4e68f1d66d083ec41b95dc9ee43d
ZhangChao <zhangchao@mac_mini.com>
2015-01-17 23:53:43 +0800

    修改： 计时控件增加暂停功能；

--- GPSRecorder/ui/TimeLabel.m
+++ GPSRecorder/ui/TimeLabel.m
@@ -17,36 +17,49 @@
     return self;
 }
 
+- (bool) isInitialized {
+    if (_mTimer) {
+        return true;
+    }
+    return false;
+}
 - (void)updateLabel {
     NSTimeInterval interval = [_mStartedDate timeIntervalSinceNow];
-    NSLog(@"timerFireMethod timeIntervalSinceNow : %f", fabs(interval));
-    self.text = [NSString stringWithFormat:@"%d", (int)(floor(fabs(interval) + 0.5))];
+    _curTimeInterval = floor(fabs(interval) + 0.5);
+    self.text = [NSString stringWithFormat:@"%d", (int)(_lastTimeInterval + _curTimeInterval)];
 }
 
 - (void)initTimer {
+    NSLog(@"initTimer");
     if (_mTimer) {
-        [_mTimer invalidate];
-        _mTimer = nil;
+        [self resetTimer];
     }
     _mTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateLabel) userInfo:nil repeats:YES];
     [_mTimer setFireDate:[NSDate distantFuture]]; // pause
 }
 
 - (void)startTimer {
-    if (!_mTimer) {
+    if (![self isInitialized]) {
         [self initTimer];
     }
     [_mTimer setFireDate:[NSDate distantPast]]; // start
     _mStartedDate = [NSDate date];
+    _isTimerRunning = true;
 }
 
 - (void)pauseTimer {
     [_mTimer setFireDate:[NSDate distantFuture]]; // pause
+    _lastTimeInterval = _lastTimeInterval + _curTimeInterval;
+    _isTimerRunning = false;
 }
 
 - (void)resetTimer {
     [_mTimer invalidate];
     _mTimer = nil;
+    self.text = @"0";
+    _isTimerRunning = false;
+    _lastTimeInterval = 0;
+    _curTimeInterval = 0;
 }
 
 @end
